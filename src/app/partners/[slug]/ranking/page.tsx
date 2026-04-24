f603fc36a02264736bc9c05568a5b50bb3fdde1f
Kauã Guerrero <kauaguerrerom@gmail.com>
2026-04-24 19:57:11 -0300

    Sidebar Founder Ranking Fix

---
+++ src/app/partners/[slug]/ranking/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { PartnerLayout } from '@/components/partners/PartnerLayout';
+import RankingContent from '../student/ranking/page';
+
+export default function FounderRankingPage() {
+  return (
+    <PartnerLayout>
+      <RankingContent />
+    </PartnerLayout>
+  );
+}
